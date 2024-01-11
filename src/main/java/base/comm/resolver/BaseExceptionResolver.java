010d727b14617b7793f9875b8db631990ade2122
ojh <kfetuss@gmail.com>
2024-01-11 10:52:07 +0900

    Exception 처리 추가 스케쥴 쿼리 수정

---
+++ src/main/java/base/comm/resolver/BaseExceptionResolver.java
@@ -0,0 +1,31 @@
+package base.comm.resolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+public class BaseExceptionResolver extends SimpleMappingExceptionResolver{
+
+	@Override
+	protected ModelAndView doResolveException(
+			HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+		//에러 로그 출력
+		logger.error("¿¿¿¿¿¿¿¿¿¿¿¿ BaseExceptionResolver.doResolveException 에러로그 ¿¿¿¿¿¿¿¿¿¿¿¿");
+		ex.printStackTrace();
+		
+		String viewName = determineViewName(ex, request);
+		if (viewName != null) {
+			Integer statusCode = determineStatusCode(request, viewName);
+			if (statusCode != null) {
+				applyStatusCodeIfPossible(request, response, statusCode);
+			}
+			return getModelAndView(viewName, ex, request);
+		}
+		else {
+			return null;
+		}
+	}
+}
